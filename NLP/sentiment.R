25ae066340d7cf948f7e0253e1b4f314cf92e5d6
M <michael.j.liu@telus.com>
2015-09-15 10:13:15 -0700

    R version of the sample code - batch exectuion

---
+++ NLP/sentiment.R
@@ -0,0 +1,28 @@
+require(httr)
+require(jsonlite)
+
+# account key here
+# account key shown on the http://datamarket.azure.com my account page
+account_key <- "AccountKey:<your account key here>"
+secret <- RCurl::base64(account_key)
+
+# the sentiment batch URL
+sentiment_url <- 'https://api.datamarket.azure.com/data.ashx/amla/text-analytics/v1/GetSentimentBatch'
+
+# assuming the text in the data frame named sentiment.text
+# column names: Id, Text
+params <- paste("{Inputs:", toJSON(sentiment.text), "}")
+
+req <- POST(sentiment_url, 
+            add_headers(
+                        "Content-Type"  = paste("application/json"),
+                        "Authorization" = paste("Basic", secret)
+                        ),
+            body = paste(params)
+            )
+
+#token <- paste("Bearer", content(req)$access_token)
+
+# parse the result into data frame
+json <- content(req, as="text")
+sentiments <- fromJSON(json)
